ee29ed11710fc797da1233ff54894923b8544939
Daniel Bauer <dan717@gmx.de>
2024-01-19 18:12:27 +0100

    Added functionality and menu to toggle light and dark mode

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -8,19 +8,26 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    // So wird herausgefunden, ob der Computer des Users einen Theme Mode eingestellt hat. Wenn nicht, wird der Theme Mode vom Local Storage genommen.
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      // Ist das der Fall, wird der Theme Mode auf dark gesetzt und der CSS Class dark der Classlist des Browsers hinzugefügt
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      // Ist das nicht der Fall, wird der Theme Mode auf light gesetzt und die CSS Class dark von der Classlist des Browsers entfernt
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
   // Beim Start der App wird der Theme Mode aus dem Local Storage geholt und gesetzt
-  //   useEffect(() => {
-  //     handleThemeChange();
-  //   }, [mode]);
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
 
   // Der Provider muss hier returned werden zusammen mit den State Werten, welche hier gesetzt wurden
   return (
@@ -29,7 +36,8 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
     </ThemeContext.Provider>
   );
 }
-// Der Hook wird hier exportiert, damit er in anderen Dateien verwendet werden kann
+// Der Hook wird hier exportiert, damit er in anderen Dateien verwendet werden kann.
+//  Wo kann von dort schnell auf das State mode zugegriffen werden
 export function useTheme() {
   const context = useContext(ThemeContext);
 
