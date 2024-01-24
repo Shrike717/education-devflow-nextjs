df27de3e5ab3359a5c5fd1b18876ef87cdf4254b
Daniel Bauer <dan717@gmx.de>
2024-01-24 16:35:32 +0100

    Added file for Question server actions

---
+++ lib/actions/question.action.ts
@@ -0,0 +1,8 @@
+"use Server";
+// Hier stehen alle Server Actions für das Questions Model
+
+export async function createQuestions(params) {
+  try {
+    // Diese Funktion muss ich zur Datenbank verbinden
+  } catch (error) {}
+}
