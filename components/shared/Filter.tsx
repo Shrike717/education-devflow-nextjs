038e12016cddcfc4051ef4e9fe531bd96bcd6265
Daniel Bauer <dan717@gmx.de>
2024-01-28 14:00:54 +0100

    Fixed type error in Filter component

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -17,13 +17,7 @@ interface Props {
   containerClasses?: string;
 }
 
-const Filter = ({
-  filters,
-  otherClasses,
-  containerClasses,
-}: {
-  filters: Props;
-}) => {
+const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
   return (
     <div className={`relative ${containerClasses}`}>
       <Select>
