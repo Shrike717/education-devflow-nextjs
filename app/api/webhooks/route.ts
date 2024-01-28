8ed0e0e28167f9577dd9085934ed3312da104e06
Daniel Bauer <dan717@gmx.de>
2024-01-28 11:31:06 +0100

    Added the listeners for the 3  Clerk user events create, update and delete in our endpoint route handler. Also added all needed server actions for  the user.

---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,128 @@
+// This code was copied from Clerk
+
+/* eslint-disable camelcase */
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+  // TODO: Add secret to .env
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+    );
+  }
+
+  // Get the headers.
+  // These are needed to ensure that the request is coming from Clerk.
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
+  }
+
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: WebhookEvent;
+
+  // Verify the payload with the headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
+
+  // Get the type oo the event:
+  const eventType = evt.type;
+
+  //   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  //   console.log("Webhook body:", body);
+
+  // Here we want to do something with the events.
+
+  if (eventType === "user.created") {
+    // Get the user data from the event
+    const { id, email_addresses, image_url, username, first_name, last_name } =
+      evt.data;
+
+    // Call a server action to create a user in the database:
+    const mongoUser = await createUser({
+      clerkId: id,
+      name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+      username: username!, // We know that username is not null because we are creating a user with Clerk
+      email: email_addresses[0].email_address,
+      picture: image_url,
+    });
+
+    // We return a Next response with the user data:
+    return NextResponse.json({
+      message: "User successfully created",
+      user: mongoUser,
+    });
+  }
+
+  if (eventType === "user.updated") {
+    // Get the user data from the event
+    const { id, email_addresses, image_url, username, first_name, last_name } =
+      evt.data;
+
+    // Call a server action to create a user in the database:
+    const mongoUser = await updateUser({
+      clerkId: id,
+      updatedData: {
+        name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+        username: username!, // We know that username is not null because we are creating a user with Clerk
+        email: email_addresses[0].email_address,
+        picture: image_url,
+      },
+      path: `/profile/${id}`, // We have to let Next which page has to be regenerated after the user is updated
+    });
+
+    // We return a Next response with the user data:
+    return NextResponse.json({
+      message: "User successfully updated",
+      user: mongoUser,
+    });
+  }
+
+  if (eventType === "user.deleted") {
+    // Get the user id from the event
+    const { id } = evt.data;
+
+    // Call a server action to delete a user in the database:
+    const deletedUser = await deleteUser({
+      clerkId: id!,
+    });
+
+    // We return a Next response with the user data:
+    return NextResponse.json({
+      message: "User successfully deleted",
+      user: deletedUser,
+    });
+  }
+
+  return new Response("", { status: 200 });
+}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,16 +2,25 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  GetUserByIdParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 // Here we have all the actions for the users model:
 
-export async function getUserById(params: any) {
+// GET ONE USER BY ID
+export async function getUserById(params: GetUserByIdParams) {
   //   console.log("[getUserById] params:", params);
   try {
     // Connect to the database:
     await connectToDatabase();
 
-    // Then we have to destruct the params:
+    // Then we have to destructure the params:
     const { userId } = params;
 
     // Get the user. We use findOne because we want to find one user. We want to find the user by the clerkId:
@@ -19,6 +28,93 @@ export async function getUserById(params: any) {
 
     // Return the user:
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// CREATE A USER
+export async function createUser(userData: CreateUserParams) {
+  //   console.log("[createUser] params:", params);
+  try {
+    // Connect to the database:
+    await connectToDatabase();
+
+    // Create the user:
+    const newUser = await User.create({
+      userData,
+    });
+
+    // Return the user:
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// UPDATE A USER
+export async function updateUser(params: UpdateUserParams) {
+  //   console.log("[createUser] params:", params);
+  try {
+    // Connect to the database:
+    await connectToDatabase();
+
+    // Then we have to destructure the params:
+    const { clerkId, updatedData, path } = params;
+
+    // Update the user:
+    await User.findOneAndUpdate(
+      { clerkId }, // We want to find the user by the clerkId
+      updatedData, // Then We want to update the user with the updatedData
+      { new: true }
+    );
+
+    // We have to let Next which page has to be regenerated after the user is updated
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// DELETE A USER
+export async function deleteUser(params: DeleteUserParams) {
+  //   console.log("[createUser] params:", params);
+  try {
+    // Connect to the database:
+    await connectToDatabase();
+
+    // Then we have to destructure the params:
+    const { clerkId } = params;
+
+    // Find the user:
+    const user = await User.findById({ clerkId }); // We want to find the user by the clerkId
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    //  We have to delete the user from the database:
+    // And everything related to the user like the questions, answers, comments, votes, etc
+
+    // We delete the questions of the user:
+    // Get user's questions ids:
+    const userQuestionsIds = await Question.find({ author: user._id }).distinct(
+      "_id"
+    ); //  Creates a distinct query: returns the distinct values of the given field that match filter.
+
+    // Delete the questions:
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: Delete the answers, comments of the user
+
+    // Finally we delete the user:
+    const deletedUser = await User.findByIdAndDelete(user._id); // We want to find the user by the clerkId
+
+    // Return the deleted user:
+    return deletedUser;
   } catch (error) {
     console.log(error);
     throw error;
