915b9a1b47d897655d241b5254fbb784c8975083
Daniel Bauer <dan717@gmx.de>
2024-01-31 14:22:39 +0100

    Changed code in ask-question page. monoUseer can be null

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -23,7 +23,9 @@ const Page = async () => {
     <div>
       <h1 className="h1-bold text-dark100_light900 ">Ask a question</h1>
       <div className="mt-9">
-        <Question mongoUser={JSON.stringify(mongoUser._id)} />
+        <Question
+          mongoUser={mongoUser ? JSON.stringify(mongoUser._id) : null}
+        />
       </div>
     </div>
   );
