2178e0db9797849b8b9b2b2701580ee65fd17b97
Daniel Bauer <dan717@gmx.de>
2024-01-31 14:55:10 +0100

    And changed last commit back again

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -126,7 +126,7 @@ export async function deleteUser(params: DeleteUserParams): Promise<IUser> {
 // GET ALL USERS
 export async function getAllUsers(
   params: GetAllUsersParams
-): Promise<{ users: IUser[] }> {
+): Promise<{ users: User[] }> {
   try {
     // Connect to the database:
     await connectToDatabase();
