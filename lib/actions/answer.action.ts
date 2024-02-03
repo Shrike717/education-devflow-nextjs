29e9ff7e4368fe7f0d57beb3165db9ed15890469
Daniel Bauer <dan717@gmx.de>
2024-02-03 18:44:14 +0100

    Displayed all answers belonging to the question  in a reusable component AllAnswers on question detail page

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -2,9 +2,10 @@
 
 import Answer from "@/database/answer.model";
 import { connectToDatabase } from "../mongoose";
-import { CreateAnswerParams } from "./shared.types";
+import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
 import Question from "@/database/question.model";
 import { revalidatePath } from "next/cache";
+import User from "@/database/user.model";
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
@@ -26,6 +27,28 @@ export async function createAnswer(params: CreateAnswerParams) {
     // TODO: Add interaction....
 
     revalidatePath(path); // We revalidate the path to update the cache. This will cause the page to be revalidated and updated.
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAnswers(params: GetAnswersParams) {
+  try {
+    connectToDatabase();
+
+    // We need to destructure the questionId from the params object for which we want to get the answers.
+    const { questionId } = params;
+
+    const answers = await Answer.find({ question: questionId })
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id clerkId name picture",
+      })
+      .sort({ createdAt: -1 });
+
+    return { answers };
   } catch (error) {
     console.log(error);
     throw error;
