3bfc974210e153b24d53274b7eb1c6ff0556aa27
Daniel Bauer <dan717@gmx.de>
2024-03-05 15:36:54 +0100

    Implemented the functionality to dynamically get and delete the values from the search field in LocalSearchbar and build a query with the values.

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { Input } from "@/components/ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 import Image from "next/image";
-import React from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 interface CustomInputProps {
   route: string;
@@ -18,6 +20,50 @@ const LocalSearchbar = ({
   placeholder,
   otherClasses,
 }: CustomInputProps) => {
+  // Things we need for the search functionality:
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  // When we have the searchParams, we can access the query:
+  const query = searchParams.get("q");
+  //   console.log("query", query);
+  //   console.log("[searchParams.toString]", searchParams.toString());
+
+  // Then we set a local state for the query. Every key stroke will update the state:
+  const [search, setSearch] = useState(query || "");
+
+  // To get the values from the field into the URL, we use useEffect:
+  useEffect(() => {
+    // We want to have a function for the debounce effect. A request for every letter is too much. Can crash the browser.
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "q",
+          value: search,
+        });
+
+        // Then we want to push the new URL to the router:
+        router.push(newUrl, { scroll: false });
+      } else {
+        // If the search is empty, we want to remove the query from the URL:
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["q"],
+          });
+
+          // Then we want to push the new URL to the router:
+          router.push(newUrl, { scroll: false });
+        }
+      }
+
+      // We want to clear the timeout after every letter and also call the function again:
+      return () => clearTimeout(delayDebounceFn);
+    }, 300);
+  }, [search, route, pathname, router, searchParams, query]);
+
   return (
     <div
       className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
@@ -36,8 +82,8 @@ const LocalSearchbar = ({
       <Input
         type="text"
         placeholder={placeholder}
-        value=""
-        onChange={() => {}}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
       />
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,10 +2,12 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import qs from "query-string";
+
 // This code belongs to the configuration of ShadCn. Was created automatically during initialization
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
+} // This is a library to parse and stringify URL query strings
 
 // Helper function to generate a timestamp from a date.
 // Returns the timestamp as a string.
@@ -51,3 +53,57 @@ export function getJoinedDate(date: Date): string {
   const year = date.getFullYear();
   return `${month} ${year}`;
 }
+
+interface UrlQueryParams {
+  params: string;
+  key: string;
+  value: string | null;
+}
+
+// This function checks the curreent URL. Maybe we have some filters, categories etc.
+// It keeps all of those and only updates the key we are interested in while keeping the rest.
+export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
+  // First we want to get access to the current URL:
+  const currentUrl = qs.parse(params);
+
+  // Then we want to set the new value for the key we are updating:
+  currentUrl[key] = value;
+
+  // Then we want to stringify and return the new URL:
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname, // Here we first get the base pathname
+      query: currentUrl, // Then we add the query
+    },
+    { skipNull: true } // This is to skip null values
+  );
+};
+
+interface RemoveUrlQueryParams {
+  params: string;
+  keysToRemove: string[];
+}
+
+// This function checks the current URL. Maybe we have some filters, categories etc.
+// It rremoves the key of the query when we delete the values in the local search bar.
+export const removeKeysFromQuery = ({
+  params,
+  keysToRemove,
+}: RemoveUrlQueryParams) => {
+  // First we want to get access to the current URL:
+  const currentUrl = qs.parse(params);
+
+  // Then we want to remove the keys we are not interested in:
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
+
+  // Then we want to stringify and return the new URL:
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname, // Here we first get the base pathname
+      query: currentUrl, // Then we add the query
+    },
+    { skipNull: true } // This is to skip null values
+  );
+};
