1812db6c4ac5c681e8a34b7024e5656d23b7fa01
Daniel Bauer <dan717@gmx.de>
2024-03-11 13:08:07 +0100

    Implemented functionality to set global search term to the url and crated  basic GlobalRessult cmponent

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,7 +1,48 @@
+"use client";
+
 import Image from "next/image";
+import React, { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import GlobalResult from "./GlobalResult";
 
 const GlobalSearch = () => {
+  // Things we need for the search functionality:
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get("q"); // When we have the searchParams, we can access the local query:
+
+  const [search, setSearch] = useState(query || ""); // Then we set a local state for the query. Every key stroke will update the state
+  const [isOpen, setIsOpen] = useState(false); // We want to have a local state for the modal
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "global",
+          value: search,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (query) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["global", "type"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, router, pathname, searchParams, query]);
+
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
@@ -10,16 +51,23 @@ const GlobalSearch = () => {
           alt="search"
           width={24}
           height={24}
-          className="cursor-pointer "
+          className="cursor-pointer"
         />
 
         <Input
           type="text"
           placeholder="Search globally"
-          //   value=""
-          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none "
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+
+            if (!isOpen) setIsOpen(true); // Here we have to check if the modal is open. If not, we want to open it
+            if (e.target.value === "" && isOpen) setIsOpen(false); // If the input is empty and the modal is open, we want to close it
+          }}
+          className="paragraph-regular no-focus placeholder text-dark400_light700 background-light800_darkgradient border-none shadow-none outline-none"
         />
       </div>
+      {isOpen && <GlobalResult />}
     </div>
   );
 };
