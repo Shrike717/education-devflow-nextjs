1808d900f48e08d0a79de85ff15b8dac8bb3b1b6
Daniel Bauer <dan717@gmx.de>
2024-03-18 17:44:43 +0100

    Implemented Skeleton loader for community page

---
+++ app/(root)/community/loading.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const Loading = () => {
+  return (
+    <section>
+      <h1 className="h1-bold text-dark100_light900 ">All Users</h1>
+      <div className="mb-12 mt-11 flex flex-wrap gap-5">
+        <Skeleton className="h-14 flex-1" />
+        <Skeleton className="h-14 " />
+      </div>
+
+      <div className="flex flex-wrap gap-4">
+        {Array.from({ length: 9 }).map((item) => (
+          <Skeleton
+            key={item}
+            className="h-60 w-full rounded-xl sm:w-[260px]"
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Loading;
--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -6,6 +6,7 @@ import { UserFilters } from "@/constants/filters";
 import { getAllUsers } from "@/lib/actions/user.action";
 import { SearchParamsProps } from "@/types";
 import Link from "next/link";
+import Loading from "./loading";
 
 const Page = async ({
   searchParams,
@@ -16,6 +17,11 @@ const Page = async ({
     filter: searchParams.filter,
     page: searchParams.page ? +searchParams.page : 1, // The page number is taken from the URL query parameter. +searchParams.page is changing it to a number. If it's not there, the default value is 1.
   });
+
+  // Fake loading variable to simulate loading state
+  //   const isLoading = true;
+
+  //   if (isLoading) return <Loading />;
 
   return (
     <>
