aebd9c1884c221e3fbbef2a0a018699b40372e80
Daniel Bauer <dan717@gmx.de>
2024-03-22 10:32:44 +0100

    Deployment bugfix: type skeleton loader key

--- app/(root)/community/loading.tsx
+++ app/(root)/community/loading.tsx
@@ -10,7 +10,7 @@ const Loading = () => {
       </div>
 
       <div className="flex flex-wrap gap-4">
-        {Array.from({ length: 9 }).map((item) => (
+        {Array.from({ length: 9 }).map((item: number) => (
           <Skeleton
             key={item}
             className="h-60 w-full rounded-xl sm:w-[260px]"
