c910b9d4e07b42e74ef61c011fe3fc6029f4bbb5
Daniel Bauer <dan717@gmx.de>
2024-04-09 17:29:57 +0200

    Added header code to next.coonfig.js file to check whether cors error is solved when genrating ai

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     mdxRs: true,
+//     serverComponentsExternalPackages: ["mongoose"],
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "*", // Right now we set a wildcard, but you can put the domain of your API here
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "*", // Right now we set a wildcard, but you can put the domain of your API here
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -15,6 +37,22 @@ const nextConfig = {
         hostname: "*", // Right now we set a wildcard, but you can put the domain of your API here
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
   },
 };
 
